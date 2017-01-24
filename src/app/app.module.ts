aa1ccc27c31f0d95f746207f671e134914f4ad2a
Juha Selonen <juha.selonen@outlook.com>
2017-01-24 09:40:56 +0200

    Test comment 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { TestcomComponent } from './testcom/testcom.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TestcomComponent
   ],
   imports: [
     BrowserModule,
